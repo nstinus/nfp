962276c0254ffd9f8b5244d62dd5e952d039bfbb
Nicolas Stinus <nicolas.stinus@gmail.com>
2009-07-07 17:29:26 +0200

    UserMeanAlog. Here is the forgotten code.

---
+++ src/libnfpalgos/UserMeanAlgo.cpp
@@ -0,0 +1,62 @@
+#include "UserMeanAlgo.h"
+#include "RatingsManager.h"
+#include "Rating.h"
+
+void NFP::algos::UserMeanAlgo::init(){
+    LOG(INFO) << "Init...";
+    ratings_begin = NFP::shm::RatingsManager::instance()->ratings_begin();
+    ratings_end = NFP::shm::RatingsManager::instance()->ratings_end();
+    LOG(INFO) << "...done";
+}
+
+int NFP::algos::UserMeanAlgo::run()
+{
+    logStart();
+    
+    init();
+    
+    std::map<uint, unsigned long long> user_summed_rates_;
+    std::map<uint, uint> user_nb_rates_;
+    
+    
+    LOG(INFO) << "Starting loop over all the ratings...";
+    int nb_processed_ratings = 0;
+    for (std::list<NFP::model::Rating*>::iterator it = ratings_begin; it != ratings_end; it++) {
+        if (*it == NULL) {
+            LOG(ERROR) << "ptr is null!";
+            return -1;
+        }
+        
+        uint user_id = (*it)->user_id();
+        ushort rate = (*it)->rate();
+        
+        if (user_summed_rates_.find(user_id) == user_summed_rates_.end()) {
+            user_summed_rates_[user_id] = 0;
+            users.push_back(user_id);
+        }
+        user_summed_rates_[user_id] += rate;
+        if (user_nb_rates_.find(user_id) == user_nb_rates_.end())
+            user_nb_rates_[user_id] = 0;
+        user_nb_rates_[user_id]++;
+        nb_processed_ratings++;
+        LOG_EVERY_N(INFO, 1000000) << "Processed " << nb_processed_ratings << " ratings";
+    }
+    
+    LOG(INFO) << "Calculating average rate per user...";
+    for (std::vector<uint>::iterator u_id = users.begin(); u_id != users.end(); u_id++) {
+        user_mean_rates_[*u_id] = (float)user_summed_rates_[*u_id] / (float)user_nb_rates_[*u_id];
+        LOG(INFO) << "user_id = " << *u_id << ", nb_ratings = " << user_nb_rates_[*u_id]
+            << ", mean_rate = " << user_mean_rates_[*u_id];
+    }
+    
+    LOG(INFO) << "nb_users = " << users.size();
+    
+    logEnd();
+    return 0;
+}
+
+float NFP::algos::UserMeanAlgo::get_predicted_rate(uint u_id, ushort m_id, std::string date = "")
+{
+    //FIXME: warnings about u_id, date, unused.
+    return user_mean_rates_.find(u_id) == user_mean_rates_.end() ? -1.0 : user_mean_rates_[u_id];
+}
---
+++ src/libnfpalgos/UserMeanAlgo.h
@@ -0,0 +1,43 @@
+#ifndef __NFP__USERMEANALGO_H__
+#define __NFP__USERMEANALGO_H__
+
+#include <map>
+#include <list>
+#include <vector>
+#include <string>
+
+#include "BaseAlgo.h"
+#include "RatingsManager.h"
+
+namespace NFP
+{
+
+namespace algos
+{
+
+class UserMeanAlgo : public BaseAlgo
+{
+
+public:
+    UserMeanAlgo() : BaseAlgo("User_Mean", "0.1") {}
+    virtual int run();
+    std::map<uint, float>* result() {return &user_mean_rates_;}
+    void* get_results() {return &user_mean_rates_;}
+    virtual float get_predicted_rate(uint, ushort, std::string);
+    std::vector<uint>::iterator users_begin() {return users.begin();}
+    std::vector<uint>::iterator users_end() {return users.end();}    
+    
+private:
+    std::map<uint, float> user_mean_rates_;
+    std::list<NFP::model::Rating*>::iterator ratings_begin;
+    std::list<NFP::model::Rating*>::iterator ratings_end;
+    std::vector<uint> users;
+    
+    void init();
+};
+
+    
+}
+}
+
+#endif // __NFP__USERMEANALGO_H__
