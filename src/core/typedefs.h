0fa67ed5cb52ef6ba396fff3d97313e31e8b343c
Nicolas Stinus <nicolas.stinus@gmail.com>
2010-12-27 00:12:56 +0100

    [core] removed dead code

--- src/core/typedefs.h
+++ src/core/typedefs.h
@@ -1,14 +1,9 @@
 #ifndef __TYPEDEFS
 #define __TYPEDEFS_H__
 
-//#include <string>
-//#include <boost/date_time/gregorian/gregorian.hpp>
-
 typedef unsigned short int ushort;
 typedef unsigned long int ulong;
 typedef unsigned char uchar;
 typedef unsigned int uint;
-//typedef boost::gregorian::date date;
-//typedef std::string string;
 
 #endif
