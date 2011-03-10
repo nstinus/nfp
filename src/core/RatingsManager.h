69a93ecb508dbced3c68a6fbcda17a4db4f31dc2
Nicolas Stinus <nicolas.stinus@gmail.com>
2011-03-10 23:26:51 +0100

    [core] Inlined RatingsIterator

--- src/core/RatingsManager.h
+++ src/core/RatingsManager.h
@@ -11,10 +11,10 @@
 
 namespace NFP
 {
-    
+
 namespace shm
 {
-    
+
 typedef std::vector<NFP::shm::RatingsShmSegment*> RatingsSegments;
 typedef RatingsSegments::const_iterator RatingsSegmentsConstIterator;
 
@@ -28,10 +28,26 @@ public:
  RatingsIterator(RatingsSegmentsConstIterator rs_it, int r_idx) :
     rs_it_(rs_it),
     r_idx_(r_idx) {}
-  NFP::model::Rating* operator*();
-  RatingsIterator& operator++(int);
-  bool operator!=(const RatingsIterator&);
-  //bool operator==(const RatingsIterator&);
+
+  NFP::model::Rating* operator*() const {
+    return (NFP::model::Rating*)((*rs_it_)->ptr() + r_idx_);
+  }
+
+  RatingsIterator& operator++(int) {
+    ++r_idx_;
+    if (r_idx_ > (*rs_it_)->nb_ratings()) {
+      ++rs_it_; r_idx_ = 0;
+    }
+    return *this;
+  }
+
+  bool operator!=(const RatingsIterator& other) const {
+    return rs_it_ != other.rs_it_ || r_idx_ != other.r_idx_;
+  }
+
+  bool operator==(const RatingsIterator& other) const {
+    return rs_it_ == other.rs_it_ && r_idx_ == other.r_idx_;
+  }
 };
 
 class RatingsManager : public base::Singleton<RatingsManager>
@@ -58,15 +74,15 @@ public:
       return RatingsIterator(segments_.end(), 0);
     }
 
-    
+
 private:
     int init(std::string arg_movie_id = "", bool feedback = false, bool preloadSegments = false);
     void rebuildLoadedSegments();
     void addSegment(RatingsShmSegment*);
-    
+
     std::string NFP_TRAINING_SET_DIR;
     std::string NFP_SHM_FILES;
-    
+
     RatingsSegments segments_;
     std::set<std::string> loadedSegments_;
 };
