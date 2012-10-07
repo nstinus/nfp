1025252f6d977c6e6493f6113a3d0505ae9d8854
Nicolas Stinus <nicolas.stinus@gmail.com>
2012-10-06 23:35:42 -0400

    [tests] fixed compilation

--- src/core/Date.h
+++ src/core/Date.h
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdint.h>
 
 namespace NFP {
   namespace utils {
--- src/core/test/Date_test.cpp
+++ src/core/test/Date_test.cpp
@@ -2,26 +2,31 @@
 
 #include "Date.h"
 
+struct DatePair
+{
+  std::string str;
+  unsigned int jd;
+};
 
-static const std::pair<std::string, unsigned int> dates[] = {
+static const DatePair dates[] = {
   {"1970-01-01", 2440588},
   {"2005-07-05", 2453557},
   {"2012-02-29", 2455987},
   {"2005-07-05", 2453557},
   {"2005-07-05", 2453557},
 };
-static const size_t dates_len = sizeof(dates)/sizeof(std::pair<std::string, unsigned int>);
+static const size_t dates_len = sizeof(dates)/sizeof(DatePair);
 
 TEST(Date, toJulianDay) {
   for (size_t i=0; i<dates_len; ++i) {
-    EXPECT_EQ(NFP::utils::toJulianDay(dates[i].first),
-              dates[i].second);
+    EXPECT_EQ(NFP::utils::toJulianDay(dates[i].str),
+              dates[i].jd);
   }
 }
 
 TEST(Date, fromJulianDay) {
   for (size_t i=0; i<dates_len; ++i) {
-    EXPECT_EQ(NFP::utils::fromJulianDay(dates[i].second), dates[i].first);
+    EXPECT_EQ(NFP::utils::fromJulianDay(dates[i].jd), dates[i].str);
   }
 }
 
